f5789c69831c76d6c3d9af911acef566bf301200
beelarr <bryonl@me.com>
2017-10-09 10:43:43 -0500

    Begin rescue blocks

--- main.rb
+++ main.rb
@@ -5,3 +5,4 @@ jordan = Student.new("Jordan")
 p "THis is Jordan the student #{jordan}."
 p "Here is his descriptoin #{jordan.description}"
 p "Does he have a car?? #{Student.has_car?}"
+jordan.student_age(28)
--- student.rb
+++ student.rb
@@ -1,7 +1,27 @@
+require_relative 'car'
+
 class Student
   include Car
   attr_accessor :name
   def initialize(name)
     @name = name
   end
+  def student_age(age)
+    puts "Before begin rescue block"
+    begin
+      puts "Hi, this is working"
+      raise ArgumentError, "Argument is not a number" unless age.is_a? Integer
+      puts "Is it still working?"
+      puts "#{name} is #{age} years old"
+    rescue ArgumentError => e
+      puts "There been an exeption : #{e.message}"
+      puts "Here is the backtrace #{e.backtrace}"
+    rescue  ZeroDivisionError => e
+      puts "Don't divide by 0"
+    else
+      p "Run this code if everything goes well"
+    end
+    p "After begin-end block of code"
+  end
 end
+
